f33816d88ddf2b86283ef28e93c4b12dd43b332b
anushka23agarwal <anushkaagarwal8@deloitte.com>
2023-11-30 17:33:18 +0530

    Updates

---
+++ precache-manifest.fbfcab945ca38c3acad28a44d8f4c160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bf045a99402e2a158b78b2187442fe",
+    "url": "/text-utils/index.html"
+  },
+  {
+    "revision": "d95e2a0fd96e4bc15e89",
+    "url": "/text-utils/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "7b652e71e8e5ebfeac4f",
+    "url": "/text-utils/static/js/2.76217893.chunk.js"
+  },
+  {
+    "revision": "d3b9b20c582091a571839814cdbd2d0b",
+    "url": "/text-utils/static/js/2.76217893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5770b6dbafba458b69",
+    "url": "/text-utils/static/js/3.9d25297e.chunk.js"
+  },
+  {
+    "revision": "d95e2a0fd96e4bc15e89",
+    "url": "/text-utils/static/js/main.aef12f7a.chunk.js"
+  },
+  {
+    "revision": "ceab326b78e0c6dd34da",
+    "url": "/text-utils/static/js/runtime-main.c0602515.js"
+  }
+]);
